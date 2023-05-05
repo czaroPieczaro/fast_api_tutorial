a59250a2a29452ed8dcf05f33e61fcc9df1df024
Cezary Dudkiewicz <cezary.dudkiewicz@gft.com>
2023-05-05 10:39:39 +0200

    2nd commit

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.base import api_router
+from fastapi.staticfiles import StaticFiles
+from db.session import engine
+from db.base import Base
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
-@app.get('/')
-def hello_api():
-    return {'msg': 'Hello API'}
+def include_router(app):
+    app.include_router(api_router)
+
+
+def configure_static(app):
+    app.mount('/static', StaticFiles(directory='static'), name='static')
+
+
+def create_tables():
+    print("create tables")
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(app)
+    configure_static(app)
+    create_tables()
+    return app
+
+
+app = start_application()
