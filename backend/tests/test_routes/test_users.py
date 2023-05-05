97ee147fce98d314a8e89f0b5c28b925ec6ed59e
Cezary Dudkiewicz <cezary.dudkiewicz@gft.com>
2023-05-05 11:15:12 +0200

    added first test + tests config

---
+++ backend/tests/test_routes/test_users.py
@@ -0,0 +1,9 @@
+import json
+
+
+def test_create_user(client):
+    data = {"username": "testuser", "email": "testuser@foobar.com", "password": "testing"}
+    response = client.post("/users/", content=json.dumps(data))
+    assert response.status_code == 200
+    assert response.json()["email"] == "testuser@foobar.com"
+    assert response.json()["is_active"] is True
